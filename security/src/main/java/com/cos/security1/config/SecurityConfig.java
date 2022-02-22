10b533601f4598cd8bec4753f3e35638eaf880c9
cousim46 <wjdghldns24@naver.com>
2022-02-22 15:23:53 +0900

    구글 로그인 후처리

--- security/src/main/java/com/cos/security1/config/SecurityConfig.java
+++ security/src/main/java/com/cos/security1/config/SecurityConfig.java
@@ -1,5 +1,7 @@
 package com.cos.security1.config;
 
+import com.cos.security1.config.oauth.PrincipalOauth2UserService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -7,18 +9,29 @@ import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+        /** 구글 로그인이 완료된 뒤의 후처리 필요
+         1. 코드 받기
+         2, 엑세스 토큰 받기(사용자 권한)
+         3. 사용자 프로필 정보를 가져옴
+         4-1.  가져온 정보를 토대로 회원가입을 자동으로 진행시키기도 함
+         4-2. 가져온 정보가 부족할 경우 부족한 정보를 채울 창 띄어줘야됨
+         */
 
 @Configuration
 @EnableWebSecurity // 스프링 시큐리티 필터가 스프링 필터체인에 등록됨
 @EnableGlobalMethodSecurity(securedEnabled = true, // secured 어노텡이션 황성화
-                              prePostEnabled = true  )  // preAuthorize 어노테이션 활성화
+        prePostEnabled = true)  // preAuthorize 어노테이션 활성화
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Autowired
+    private PrincipalOauth2UserService principalOauth2UserService;
 
 
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable();
@@ -34,6 +47,9 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .defaultSuccessUrl("/")
                 .and()
                 .oauth2Login()
-                .loginPage("/loginForm");
+                .loginPage("/loginForm") // 구글 로그인이 완료된 뒤의 후처리 필요 Tip : 구글 로그인이 되면 코드x  액세스토큰 사용자 프로필 정보를 받느다
+                .userInfoEndpoint()
+                .userService(principalOauth2UserService);
+
     }
 }
