763d7d8d2b00500d528783abe3de0236246981df
cousim46 <wjdghldns24@naver.com>
2022-02-24 16:43:32 +0900

    jwt를 이용하기 위한 시큐리티 설정

---
+++ jwt/src/main/java/com/cos/jwt/config/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.cos.jwt.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.web.filter.CorsFilter;
+
+@Configuration
+@EnableWebSecurity
+@RequiredArgsConstructor
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    private final CorsFilter corsFilter;
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable();
+        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용 안하겠다 무상태로 만들겠다
+                .and()
+                .addFilter(corsFilter) // @CrossOrigin는 인증이 없을때 인증이 있을대는 filter에 등록
+                .formLogin().disable() // form 로그인 사용 x
+                .httpBasic().disable()
+                .authorizeRequests()
+                .antMatchers("api/v1/user/**")
+                .access("hasRole('ROLE_USER') or hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("api/v1/manager/**")
+                .access("hasRole('ROLE_MANAGER') or hasRole('ROLE_ADMIN')")
+                .antMatchers("api/v1/admin/**")
+                .access("hasRole('ROLE_ADMIN')")
+                .anyRequest().permitAll();
+
+
+    }
+}
